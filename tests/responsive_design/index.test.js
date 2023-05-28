b0ba1ac7e55345c11d455eb5b20f000e13711bf2
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2023-05-28 11:30:59 +0300

    resp

--- tests/responsive_design/index.test.js
+++ tests/responsive_design/index.test.js
@@ -4,55 +4,55 @@
 
 // const { coffees } = require("../data");
 // const { renderCoffee } = require("../../main");
-const { default: puppeteer } = require("puppeteer");
+// const { default: puppeteer } = require("puppeteer");
 
-describe("Responsive Design", () => {
-  let page;
+// describe("Responsive Design", () => {
+//   let page;
 
-  beforeAll(async () => {
-    // Set up your testing environment to simulate the desired screen size
-    // Example: Use Puppeteer to launch a headless browser with specific viewport dimensions
-    const browser = await puppeteer.launch({ headless: true });
-    page = await browser.newPage();
-    await page.setViewport({ width: 768, height: 1024 });
+//   beforeAll(async () => {
+//     // Set up your testing environment to simulate the desired screen size
+//     // Example: Use Puppeteer to launch a headless browser with specific viewport dimensions
+//     const browser = await puppeteer.launch({ headless: true });
+//     page = await browser.newPage();
+//     await page.setViewport({ width: 768, height: 1024 });
 
-    // Navigate to your application or load the relevant HTML file
-    await page.goto("./../../index.html");
-  });
+//     // Navigate to your application or load the relevant HTML file
+//     await page.goto("./../../index.html");
+//   });
 
-  afterAll(async () => {
-    // Close the browser or perform necessary cleanup
-    await page.close();
-    await browser.close();
-  });
+//   afterAll(async () => {
+//     // Close the browser or perform necessary cleanup
+//     await page.close();
+//     await browser.close();
+//   });
 
-  // test("Header navigation is responsive", async () => {
-  //   // Simulate a smaller screen size
-  //   await page.setViewport({ width: 320, height: 480 });
+//   // test("Header navigation is responsive", async () => {
+//   //   // Simulate a smaller screen size
+//   //   await page.setViewport({ width: 320, height: 480 });
 
-  //   // Access and interact with elements on the page
-  //   const header = await page.$(".header");
-  //   const menuButton = await header.$(".menu-button");
+//   //   // Access and interact with elements on the page
+//   //   const header = await page.$(".header");
+//   //   const menuButton = await header.$(".menu-button");
 
-  //   // Verify the expected behavior of your responsive design
-  //   expect(menuButton).not.toBeNull();
-  //   expect(menuButton).toHaveClass("visible-on-mobile");
-  // });
+//   //   // Verify the expected behavior of your responsive design
+//   //   expect(menuButton).not.toBeNull();
+//   //   expect(menuButton).toHaveClass("visible-on-mobile");
+//   // });
 
-  test("Content layout adapts at different breakpoints", async () => {
-    // Simulate a larger screen size
-    await page.setViewport({ width: 1280, height: 800 });
+//   test("Content layout adapts at different breakpoints", async () => {
+//     // Simulate a larger screen size
+//     await page.setViewport({ width: 1280, height: 800 });
 
-    // Access and interact with elements on the page
-    const sidebar = await page.$(".coffee");
-    // const mainContent = await page.$(".main-content");
+//     // Access and interact with elements on the page
+//     const sidebar = await page.$(".coffee");
+//     // const mainContent = await page.$(".main-content");
 
-    console.log(sidebar);
+//     console.log(sidebar);
 
-    // Verify the expected behavior of your responsive design
-    // expect(sidebar).toHaveClass("visible-on-desktop");
-    // expect(mainContent).toHaveClass("expanded-on-desktop");
-  });
+//     // Verify the expected behavior of your responsive design
+//     // expect(sidebar).toHaveClass("visible-on-desktop");
+//     // expect(mainContent).toHaveClass("expanded-on-desktop");
+//   });
 
-  // Add more tests for other breakpoints or responsive components
-});
+//   // Add more tests for other breakpoints or responsive components
+// });
